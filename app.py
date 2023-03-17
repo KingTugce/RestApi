4fa650973cfa2273eb35a59c2b13ff7a09fb3e2b
KingTugce <kingstugce@gmail.com>
2023-03-17 01:23:02 -0700

    update

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def create_store():
 @app.post("/store/<string:name>/item")
 def create_item(name):
     request_data = request.get_json()
-    for store in stores: if store["name"] == name:
+    for store in stores: 
+        if store["name"] == name:
             new_item = {"name": request_data["name"], "price": request_data["price"]}
             store["items"].append(new_item)
             return new_item, 201
